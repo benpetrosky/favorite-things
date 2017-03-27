e5a534951451d265866ccab8be514ccdabfc76f4
Ben Petrosky and Ilene Gorski <ben@>
2017-03-27 11:15:40 -0700

    simpify javascript using loops example

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,21 @@ $(function() {
     var animal = $("input:radio[name=animal]:checked").val();
 
     var favoriteThings = [color, food, animal];
-    // alert(favoriteThings);
     var evenMoreFavoriteThings = [];
     evenMoreFavoriteThings.push(favoriteThings[2], favoriteThings[0], favoriteThings[1]);
-
-    // alert(evenMoreFavoriteThings.pop());
 
   $("#foodReturn").text(favoriteThings[1]);
   $("#colorReturn").text(favoriteThings[0]);
   $("#animalReturn").text(favoriteThings[2]);
   $(".return").show();
   });
+
+  var tags =["h3", "h1", "label"]
+
+  tags.forEach(function(tag){
+    $(tag).click(function(){
+      alert("this is a " + tag + " tag.")
+    });
+  });
+
 });
