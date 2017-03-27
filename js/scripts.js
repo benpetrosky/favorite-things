c238f3f76bf62953c043443773419de4631571c6
Ben Petrosky and Ilene Gorski <ilene@>
2017-03-27 10:18:51 -0700

    Add list of input values

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(function() {
-  $("form").submit(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
 
     var color = $("#color").val();
     var food = $("#food").val();
     var animal = $("input:radio[name=animal]:checked").val();
 
-    var favoriteThings = [color + ", " + food + ", " + animal];
-    alert(favoriteThings);
+    var favoriteThings = [color, food, animal];
+    // alert(favoriteThings);
+    var evenMoreFavoriteThings = [];
+    evenMoreFavoriteThings.push(favoriteThings[2], favoriteThings[0], favoriteThings[1]);
 
+    // alert(evenMoreFavoriteThings.pop());
+
+  $("#foodReturn").text(favoriteThings[1]);
+  $("#colorReturn").text(favoriteThings[0]);
+  $("#animalReturn").text(favoriteThings[2]);
+  $(".return").show();
   });
 });
